7fcf3bd0783bcd0bb4bee933bc6707d1c8068f3a
Georges <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 02:35:31 +0000

    ci-cd 3

--- .github/scripts/update_index.sh
+++ .github/scripts/update_index.sh
@@ -6,12 +6,12 @@ timestamp=$(date)
 output="Data.txt Frequency Analysis: $timestamp<br>${output//$'\n'/<br>}"
 
 # Append the output after the closing ul-tag in index.html using a here document
-sed -i '/<\/ul>/r /dev/stdin' /app/index.html << EOM
+sed -i '/<\/ul>/r /dev/stdin' index.html << EOM
 <pre>$output</pre>
 EOM
 
 # Commit the changes and specify the github actor and email
 git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
-git add /app/index.html
+git add index.html
 git commit -m "Update index.html"
 git push origin main
--- .github/scripts/update_readme.sh
+++ .github/scripts/update_readme.sh
@@ -4,12 +4,12 @@
 output=$1
 
 # Append the output after the closing ul-tag in README.md using a here document
-sed -i '/<\/ul>/r /dev/stdin' /app/README.md << EOM
+sed -i '/<\/ul>/r /dev/stdin' README.md << EOM
 <pre>$output</pre>
 EOM
 
 # Commit the changes - specify the github actor and email
 git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
-git add /app/README.md
+git add README.md
 git commit -m "Update README.md"
 git push origin main
