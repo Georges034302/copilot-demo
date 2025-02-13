b0006cdb0d5fda637c983143519068daab4723b1
Georges <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 02:47:26 +0000

    ci-cd 6

--- .github/scripts/update_readme.sh
+++ .github/scripts/update_readme.sh
@@ -3,13 +3,8 @@
 # Capture the output from the argument
 output=$1
 
-# Debugging information
-echo "Updating README.md with output: $output"
-
-# Append the output after the closing ul-tag in README.md using a here document
-sed -i '/<\/ul>/r /dev/stdin' /app/README.md << EOM
-<pre>$output</pre>
-EOM
+# Append the output to README.md
+echo -e "\n<pre>$output</pre>" >> /app/README.md
 
 # Commit the changes - specify the github actor and email
 git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
--- README.md
+++ README.md
@@ -4,18 +4,3 @@ GitHub testing repo for copilot
 ## <a href="https://georges034302.github.io/copilot-demo/">Frequency Analysis Log</a>
 
 
-
-
-* Data.txt Frequency Analysis (Thu Feb 13 01:52:06 UTC 2025)
-a --> 25
-e --> 56
-i --> 38
-o --> 43
-u --> 11
-
-* Data.txt Frequency Analysis (Thu Feb 13 01:52:19 UTC 2025)
-a --> 25
-e --> 56
-i --> 38
-o --> 43
-u --> 11
